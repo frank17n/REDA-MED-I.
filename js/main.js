8210ca9a981f81947ee88d93e39ad4474ed408d0
frank <jsneiderg17@gmail.com>
2026-03-16 21:54:02 -0500

    añadir detector de limites de pantalla

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+
+const submenus = document.querySelectorAll(".dropdown2");
+
+submenus.forEach(item => {
+  item.addEventListener("mouseenter", () => {
+
+    const submenu = item.querySelector(".submenu2");
+    const rect = submenu.getBoundingClientRect();
+
+    // Si se sale por la derecha
+    if (rect.right > window.innerWidth) {
+      submenu.style.left = "auto";
+      submenu.style.right = "200px";
+    } else {
+      submenu.style.left = "200px";
+      submenu.style.right = "auto";
+    }
+
+  });
+});
+
 const dropdowns = document.querySelectorAll(".dropdown");
 
 dropdowns.forEach(drop => {
